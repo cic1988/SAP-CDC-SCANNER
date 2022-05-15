432fa09ddffa0bb7952ddc3605ec37920d5b8906
Yuan Gao <gaoyuanhot@gmail.com>
2022-05-15 15:07:13 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SAP-CDC-SCANNER
---
+++ main.py
@@ -0,0 +1,47 @@
+import requests
+import json
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+apiKey = os.getenv('APIKEY')
+secretKey = os.getenv('SECRETKEY')
+userKey = os.getenv('USERKEY')
+
+url = 'https://accounts.eu1.gigya.com'
+
+headers = {
+    'content-type': 'application/x-www-form-urlencoded',
+}
+
+## error handling
+def error_handling(err):
+    if 'errorCode' in err and err["errorCode"] != 0:
+        print(f'Got error: {err["errorCode"]} {err["errorDetails"]}')
+        return False
+
+    return True
+
+## account.search
+def account():
+    number = os.getenv('ACCOUNT_NUMBER')
+    method = 'accounts.search'
+    query = f'SELECT * FROM accounts LIMIT {number}'
+
+    data = f'apiKey={apiKey}&secret={secretKey}&userKey={userKey}&query={query}'
+
+    try:
+        response = requests.post(url + '/' + method, headers=headers, data=data)
+
+        if response.status_code == 200:
+            if error_handling(response.json()):
+                with open("data/accounts.json", "w") as f:
+                    json.dump(response.json(), f)
+
+    except requests.exceptions.RequestException as e: 
+        print('Got error: ' + e)
+        raise SystemExit(e)
+
+if __name__ == "__main__":
+    account()
